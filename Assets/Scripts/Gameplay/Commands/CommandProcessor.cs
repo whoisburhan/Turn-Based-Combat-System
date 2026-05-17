fe17bbe49108c4933cce3dda274e9b1b36ad29eb
Burhan Uddin <whoisburhan@gmail.com>
2026-05-17 20:06:41 +0600

    Implement the Command Processor

---
+++ Assets/Scripts/Gameplay/Commands/CommandProcessor.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using TurnBasedCombat.Core;
+using UnityEngine;
+
+namespace TurnBasedCombat.Gamplay
+{
+    public class CommandProcessor
+    {
+        private readonly Queue<ICommand> _commandQueue = new();
+        private bool _isProcessing;
+
+        public bool IsProcessing => _isProcessing;
+
+        public void Enqueue(ICommand command) 
+        {
+            if(command == null) throw new ArgumentNullException(nameof(command));
+            _commandQueue.Enqueue(command);
+        }
+
+        public async void ExecuteAllAsync(CancellationToken ct) 
+        {
+            if (_isProcessing) return;
+            _isProcessing = true;
+
+            try
+            {
+                while (_commandQueue.Count > 0) 
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    var command = _commandQueue.Dequeue();
+
+                    await command.ExecuteAsync(ct);
+                }
+            }
+            catch 
+            {
+                Debug.LogError("[CommanProcessor] Execution halted via cancelationToken.");
+                _commandQueue.Clear();
+                throw;
+            }
+            finally 
+            {
+                _isProcessing = false;
+            }
+        }
+    }
+}
